20af14eae20211081740d5dbe2654e56cbd6469a
baizba <baizba@hotmail.com>
2017-02-07 23:38:49 +0100

    show error if no valid users with given username found

--- src/main/java/com/xteam/discount/controller/DiscountController.java
+++ src/main/java/com/xteam/discount/controller/DiscountController.java
@@ -5,10 +5,13 @@ import com.xteam.discount.service.rest.DiscountService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,24 +28,33 @@ public class DiscountController {
     private DiscountService discountService;
 
     @RequestMapping("/api/recent_purchases/{username:.+}")
-    public List<PopularPurchase> getPopularPurchasesByUsername(@PathVariable String username) {
+    public List<PopularPurchase> getPopularPurchasesByUsername(@PathVariable String username, HttpServletResponse response) {
         LOG.debug("retrieving popular purchases for user {}", username);
         List<PopularPurchase> popularPurchases = discountService.getPopularPurchasesByUsername(username);
         LOG.debug("popular purchases REST call result: {}", popularPurchases);
+
+        if (CollectionUtils.isEmpty(popularPurchases)) {
+            try {
+                response.getOutputStream().print("User with username of " + username + " was not found");
+                response.flushBuffer();
+            } catch (IOException e) {
+                LOG.error("Can not write to response any more. Error is ", e);
+            }
+        }
         return popularPurchases;
     }
 
     //TODO this should be extracted for integration test
     private List<PopularPurchase> getProductsForTest() {
-        return new ArrayList<PopularPurchase>(){
+        return new ArrayList<PopularPurchase>() {
             {
-                add(new PopularPurchase(){
+                add(new PopularPurchase() {
                     {
                         setId(123);
                         setFace("o.o");
                         setPrice(123.56);
                         setSize(23);
-                        setRecent(new ArrayList<String>(3){
+                        setRecent(new ArrayList<String>(3) {
                             {
                                 add("yoko");
                                 add("meli");
@@ -52,13 +64,13 @@ public class DiscountController {
                     }
                 });
 
-                add(new PopularPurchase(){
+                add(new PopularPurchase() {
                     {
                         setId(456);
                         setFace("x.x");
                         setPrice(145.57);
                         setSize(28);
-                        setRecent(new ArrayList<String>(3){
+                        setRecent(new ArrayList<String>(3) {
                             {
                                 add("rale");
                                 add("zloki");
@@ -68,6 +80,6 @@ public class DiscountController {
                     }
                 });
             }
-        } ;
+        };
     }
 }
